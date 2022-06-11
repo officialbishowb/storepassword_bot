25c2520d6a02d8e5427e5e0be7ca7e25e1c89a6d
officialbishowb <officialbishowb@gmail.com>
2022-06-11 22:52:58 +0200

    .

--- bot.py
+++ bot.py
@@ -181,5 +181,5 @@ async def admin_msg_handler(message: types.Message):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
     db.create_tables()
+    executor.start_polling(dp, skip_updates=True)
