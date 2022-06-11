4dd69291d7a603ecc3e7684e55be8aabb0d67a7c
officialbishowb <officialbishowb@gmail.com>
2022-06-12 00:32:38 +0200

    show user adminm

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from curses.ascii import US
 from email import message
 import db
 import functions as func
@@ -12,8 +13,9 @@ load_dotenv() # For ENV variables
 
 # GET ADMIN IDS
 
-USER_ADMIN = str(os.getenv('ADMIN_IDS')).split(',') if os.getenv('ADMIN_IDS').find(",") != -1 else os.getenv('ADMIN_IDS')
+USER_ADMIN = str(os.getenv('ADMIN_IDS')).split(',') if os.getenv('ADMIN_IDS').find(",") != -1 else str(os.getenv('ADMIN_IDS'))
 USER_ADMIN = [int(id) for id in USER_ADMIN if id != '']
+print(USER_ADMIN)
 
 API_TOKEN = os.getenv('BOT_TOKEN')
 # Configure logging
@@ -167,7 +169,6 @@ async def database_actions(message: types.Message):
 @dp.message_handler(commands=['dobackup''restoredb'])
 async def admin_msg_handler(message: types.Message):
     
-    
     if message.from_user.id in USER_ADMIN:
         print("Here")
         if(message.text.startswith("/dobackup")):
@@ -178,7 +179,6 @@ async def admin_msg_handler(message: types.Message):
         else:
             await message.reply("Send me the .db file to restore the database.")
             dp.register_next_step_handler(message, func.restore_db)
-
 
 async def restore_db(message: types.Message):
     file_id = message.document.file_id
